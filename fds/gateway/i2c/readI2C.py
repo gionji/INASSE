8a11ceaa97f50326a9863485e83792a004fd5c6d
Gionji <giovanniburresi@gmail.com>
2018-12-04 12:33:24 +0100

    added i2c read with dict

--- fds/gateway/i2c/readI2C.py
+++ fds/gateway/i2c/readI2C.py
@@ -5,27 +5,33 @@ import fds_i2c as mcus
 I2C_CHANNEL = 3
 bus = smbus.SMBus(I2C_CHANNEL)
 
-print bus.read_byte_data(mcus.I2C_ADDR_EXTERNAL, mcus.TEMP_PANEL_1)
-print bus.read_byte_data(mcus.I2C_ADDR_EXTERNAL, mcus.TEMP_PANEL_2)
-print bus.read_byte_data(mcus.I2C_ADDR_EXTERNAL, mcus.TEMP_ENV)
-print bus.read_byte_data(mcus.I2C_ADDR_EXTERNAL, mcus.PYRO)
-print bus.read_byte_data(mcus.I2C_ADDR_EXTERNAL, mcus.WIND)
+sensorsValues = dict()
 
-print bus.read_byte_data(mcus.I2C_ADDR_INTERNAL, mcus.AIR_IN)
-print bus.read_byte_data(mcus.I2C_ADDR_INTERNAL, mcus.AIR_OUT)
-print bus.read_byte_data(mcus.I2C_ADDR_INTERNAL, mcus.AIR_INSIDE)
-print bus.read_byte_data(mcus.I2C_ADDR_INTERNAL, mcus.FLOODING_STATUS)
+sensorsValues['EXTERNAL_TEMP_PANEL_1'] = bus.read_byte_data(mcus.I2C_ADDR_EXTERNAL, mcus.TEMP_PANEL_1) - 128;
+sensorsValues['EXTERNAL_TEMP_PANEL_2'] = bus.read_byte_data(mcus.I2C_ADDR_EXTERNAL, mcus.TEMP_PANEL_2) - 128;
+sensorsValues['EXTERNAL_TEMP_ENV']     = bus.read_byte_data(mcus.I2C_ADDR_EXTERNAL, mcus.TEMP_ENV) - 128;
+sensorsValues['EXTERNAL_PYRO']         = bus.read_byte_data(mcus.I2C_ADDR_EXTERNAL, mcus.PYRO) * 4;
+sensorsValues['EXTERNAL_WIND']         = bus.read_byte_data(mcus.I2C_ADDR_EXTERNAL, mcus.WIND) 
 
-print bus.read_byte_data(mcus.I2C_ADDR_IDRAULIC, mcus.PRESSURE_IN)
-print bus.read_byte_data(mcus.I2C_ADDR_IDRAULIC, mcus.PRESSURE_OUT)
-print bus.read_byte_data(mcus.I2C_ADDR_IDRAULIC, mcus.UV)
-print bus.read_byte_data(mcus.I2C_ADDR_IDRAULIC, mcus.FLUX_IN)
-print bus.read_byte_data(mcus.I2C_ADDR_IDRAULIC, mcus.FLUX_OUT)
-print bus.read_byte_data(mcus.I2C_ADDR_IDRAULIC, mcus.WATER_TEMP)
-print bus.read_byte_data(mcus.I2C_ADDR_IDRAULIC, mcus.WATER_LEVEL)
+sensorsValues['INTERNAL_AIR_IN']          = bus.read_byte_data(mcus.I2C_ADDR_INTERNAL, mcus.AIR_IN) - 128;
+sensorsValues['INTERNAL_AIR_OUT']         = bus.read_byte_data(mcus.I2C_ADDR_INTERNAL, mcus.AIR_OUT) - 128;
+sensorsValues['INTERNAL_AIR_INSIDE']      = bus.read_byte_data(mcus.I2C_ADDR_INTERNAL, mcus.AIR_INSIDE) - 128;
+sensorsValues['INTERNAL_FLOODING_STATUS'] = bus.read_byte_data(mcus.I2C_ADDR_INTERNAL, mcus.FLOODING_STATUS)
 
-print bus.read_byte_data(mcus.I2C_ADDR_ELECTRIC, mcus.CC_CURRENT)
-print bus.read_byte_data(mcus.I2C_ADDR_ELECTRIC, mcus.AC1_CURRENT)
-print bus.read_byte_data(mcus.I2C_ADDR_ELECTRIC, mcus.AC2_CURRENT)
-print bus.read_byte_data(mcus.I2C_ADDR_ELECTRIC, mcus.AC3_CURRENT)
+sensorsValues['HYDRAULIC_PRESSURE_IN']  = bus.read_byte_data(mcus.I2C_ADDR_IDRAULIC, mcus.PRESSURE_IN) * 4
+sensorsValues['HYDRAULIC_PRESSURE_OUT'] = bus.read_byte_data(mcus.I2C_ADDR_IDRAULIC, mcus.PRESSURE_OUT) * 4
+sensorsValues['HYDRAULIC_UV']           = bus.read_byte_data(mcus.I2C_ADDR_IDRAULIC, mcus.UV) * 4
+sensorsValues['HYDRAULIC_FLUX_IN']      = bus.read_byte_data(mcus.I2C_ADDR_IDRAULIC, mcus.FLUX_IN)
+sensorsValues['HYDRAULIC_FLUX_OUT']     = bus.read_byte_data(mcus.I2C_ADDR_IDRAULIC, mcus.FLUX_OUT)
+sensorsValues['HYDRAULIC_WATER_TEMP']   = bus.read_byte_data(mcus.I2C_ADDR_IDRAULIC, mcus.WATER_TEMP) - 128;
+sensorsValues['HYDRAULIC_WATER_LEVEL']  = bus.read_byte_data(mcus.I2C_ADDR_IDRAULIC, mcus.WATER_LEVEL) - 128;
+
+sensorsValues['ELECTIRC_CC_CURRENT']  = bus.read_byte_data(mcus.I2C_ADDR_ELECTRIC, mcus.CC_CURRENT)
+sensorsValues['ELECTIRC_AC1_CURRENT'] = bus.read_byte_data(mcus.I2C_ADDR_ELECTRIC, mcus.AC1_CURRENT)
+sensorsValues['ELECTIRC_AC2_CURRENT'] = bus.read_byte_data(mcus.I2C_ADDR_ELECTRIC, mcus.AC2_CURRENT)
+sensorsValues['ELECTIRC_AC3_CURRENT'] = bus.read_byte_data(mcus.I2C_ADDR_ELECTRIC, mcus.AC3_CURRENT)
+
+
+for key, value in sensorValues.items():
+	print str(key) + " :  " + str(value)
 
