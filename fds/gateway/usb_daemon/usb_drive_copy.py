b1f60088ede810679297593b0ecbe22a4e340062
gionji <giovanniburresi@gmail.com>
2019-04-30 01:17:42 +0200

    added usb removable partitions listing. TODO: copy file

--- fds/gateway/usb_daemon/usb_drive_copy.py
+++ fds/gateway/usb_daemon/usb_drive_copy.py
@@ -1,10 +1,32 @@
 import pyudev
+import psutil
+import time
 
 context = pyudev.Context()
 monitor = pyudev.Monitor.from_netlink(context)
 monitor.filter_by(subsystem='usb')
 
+
+def copyDbFileToUsbDrive():
+    print "Copy file to usb"
+    print "sync"
+    print "unmount"
+
+
 for device in iter(monitor.poll, None):
+    
+    # print ">>>>>>" + str(device.action)
+    
     if device.action == 'add':
         print('{} connected'.format(device))
-        # do something
+        
+        time.sleep(3)
+
+        removable = [device for device in context.list_devices(subsystem='block', DEVTYPE='disk') if device.attributes.asstring('removable') == "1"]
+        for device in removable:
+            partitions = [device.device_node for device in context.list_devices(subsystem='block', DEVTYPE='partition', parent=device)]
+            print("All removable partitions: {}".format(", ".join(partitions)))
+            print("Mounted removable partitions:")
+            for p in psutil.disk_partitions():
+                if p.device in partitions:
+                    print("  {}: {}".format(p.device, p.mountpoint))
