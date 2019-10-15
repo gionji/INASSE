3c10ba2069a93b75c35af39ff976169759f7b12a
Giovanni Burresi <giovanniburresi@gmail.com>
2019-10-15 13:51:14 +0200

    Removed unuseful things for tanzania to reduce space

--- fds/casottino/modbusServer/client-test01.py
+++
@@ -1,8 +0,0 @@
-import pymodbus
-from pymodbus.client.sync import ModbusTcpClient as ModbusClient
-
-IP_ADDRESS = '192.168.1.193'
-
-client = ModbusClient(IP_ADDRESS , 5020)
-a = client.read_input_registers(0,20)
-print( a.registers )
